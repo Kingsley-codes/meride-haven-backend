ac58e0fb3411e927acebcd0392017e0013182ca9
Kingsley-codes <agbamkingsley@gmail.com>
2025-09-23 20:15:15 +0100

    add vendors status

--- backend/controllers/vendorprocessingController.js
+++ backend/controllers/vendorprocessingController.js
@@ -60,7 +60,7 @@ export const fetchAllVendors = async (req, res) => {
 
         const allVendors = await Vendor.find({
             kycuploaded: true,
-        }).select('_id businessName directorID cac address phone');
+        }).select('_id businessName directorID cac address phone approvedStatus');
         res.status(200).json({ success: true, data: allVendors });
     } catch (error) {
         res.status(500).json({ success: false, message: "Server Error" });
@@ -79,7 +79,7 @@ export const approveVendor = async (req, res) => {
         }
 
         const { vendorId } = req.body;
-        const vendor = await Vendor.findById(vendorId).select('_id businessName directorID cac address phone');
+        const vendor = await Vendor.findById(vendorId).select('_id businessName directorID cac address phone approvedStatus');
         if (!vendor) {
             return res.status(404).json({ success: false, message: "Vendor not found" });
         }
@@ -104,7 +104,7 @@ export const rejectVendor = async (req, res) => {
             });
         }
 
-        const vendor = await Vendor.findById(vendorId).select('_id businessName directorID cac address phone');
+        const vendor = await Vendor.findById(vendorId).select('_id businessName directorID cac address phone approvedStatus');
         if (!vendor) {
             return res.status(404).json({ success: false, message: "Vendor not found" });
         }
